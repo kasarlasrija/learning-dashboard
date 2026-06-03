e0eb6a4034d84108e961596c156946ed10a7e838
srija <kasarlasrija369@gmail.com>
2026-06-03 15:31:48 +0530

    fix string type error

--- lib/courses.ts
+++ lib/courses.ts
@@ -1,16 +1,20 @@
-import{ supabase } from "./supabase";
-import { Course } from "../types";
-export async function getCourses(): Promise<{courses:Course[]|null, error:String | null}> {
-    try{
-        const { data, error } = await supabase
-        .from('courses')
-        .select('*')
-        .order('created_at', { ascending: true });
-        if (error) {
-            return { courses: null, error: error.message };
-        }
-        return { courses: data, error: null }
-    } catch (error) {
-        return { courses: null, error: "An unexpected error occurred" };
+import { supabase } from "./supabase"
+import { Course } from "@/types"
+
+export async function getCourses(): Promise<{ courses: Course[] | null; error: string | null }> {
+  try {
+    const { data, error } = await supabase
+      .from("courses")
+      .select("*")
+      .order("created_at", { ascending: true })
+
+    if (error) {
+      return { courses: null, error: String(error.message) }
     }
+
+    return { courses: data, error: null }
+
+  } catch (err) {
+    return { courses: null, error: String("Something went wrong. Please try again.") }
+  }
 }
